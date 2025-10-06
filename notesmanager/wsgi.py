56b6cf50204ed05678064c77bd2a6c66083ce307
shameervp15 <shameervp15@gmail.com>
2025-10-06 09:14:14 +0530

    fix: remove manual migrations

--- notesmanager/wsgi.py
+++ notesmanager/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notesmanager.settings')
 
 application = get_wsgi_application()
-
-from django.core.management import call_command
-call_command("migrate", interactive=False)
